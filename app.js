4a9f3a687c87afbf876b9df69b2c4c5b1f59ede7
maram-magdi <143852620+maram-magdi@users.noreply.github.com>
2024-01-15 05:12:53 -0800

    Testing gyroscope/accelerometer

---
+++ app.js
@@ -0,0 +1,59 @@
+let getPermissionsBttn = document.getElementById('get-permissions');
+
+let px = 50; //Position x and y
+let py = 50;
+let vx = 0;
+let vy = 0;
+let updateRate = 1/60; //sensor refresh rate?
+
+function getAccel() {
+    DeviceMotionEvent.requestPermission().then(response => {
+        if(response == 'granted') {
+            console.log("Accelerometer permission granted!");
+            //do stuff here - not sure what yet
+
+            //add a listener to get phone's acceleration 
+                //in the x,y,z axis (units in m/s2)
+                window.addEventListener('devicemotion', (event) => {
+                    console.log(event);
+                });
+            
+            // add a listener for phone's orientation 
+                //in the alpha-beta-gamma axes (units in degrees)
+                window.addEventListener('deviceorientation', (event) => {
+                    console.log(event);
+
+                    let rotationDegrees = event.alpha;
+                    let frontToBackDegrees = event.beta;
+                    let leftToRightDegrees = event.gamma;
+
+                    vx = vx + leftToRightDegrees * updateRate * 2;
+                    vy = vy + frontToBackDegrees * updateRate;
+
+                    //update position and clip it to bounds 
+                    px = px + vx * 0.5;
+                    if (px > 98 || px < 0) {
+                        px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+                        vx = 0;
+                    }
+
+                    py = py + vy * 0.5;
+                    if (py >  98 || py < 0){
+                        py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+                        vy = 0; 
+                    }
+
+                    // dot = document.getElementsByClassName("indicatorDot")[0]
+                    // dot.setAttribute('style', "left:" + (px) + "%;" +"top:" + (py) + "%;");
+                    ball.setAttribute('style', "left:" + (px) + "%;" + 
+                                        "top:" + py + "%;");
+                });
+        }
+    });
+}
+document.addEventListener('load', () => {
+
+    getPermissionsBttn.addEventListener('click', () => {
+        getAccel();
+    });
+});
