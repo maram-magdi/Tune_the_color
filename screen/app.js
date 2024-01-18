7c16e3882a86dac192316e51d19aff0fdc1711a7
maram-magdi <143852620+maram-magdi@users.noreply.github.com>
2024-01-18 08:37:42 -0800

    timer 4

--- screen/app.js
+++ screen/app.js
@@ -175,6 +175,21 @@ window.addEventListener('load', (event) => {
         if(mappedGyroValue == alphaRandom && playingSwitch == true){
 
             const timerInterval = setInterval(winAfter5, 1000);
+
+            if (playingSwitch == false){
+                
+                console.log("Winner!");
+                let statement = document.createElement('p');
+                statement.innerHTML = "Yay! You won!";
+                museumSect.appendChild(statement);
+                pixelImg.style.backgroundColor = chroma('red').alpha(alphaRandom).css();
+                jsConfetti.addConfetti({
+                    confettiColors: [
+                    '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+                    ],
+                });
+        
+            };
         
         };
       
@@ -192,18 +207,6 @@ function winAfter5 () {
         // Perform any actions you need when the timer is complete
 
         playingSwitch = false;
-            
-        console.log("Winner!");
-        let statement = document.createElement('p');
-        statement.innerHTML = "Yay! You won!";
-        museumSect.appendChild(statement);
-        pixelImg.style.backgroundColor = chroma('red').alpha(alphaRandom).css();
-        jsConfetti.addConfetti({
-            confettiColors: [
-            '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
-            ],
-        });
-
     }
             
     countdown--;
